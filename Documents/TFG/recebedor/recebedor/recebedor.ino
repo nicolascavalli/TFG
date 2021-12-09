44f2034b2616ec0e442844fac972dfad4fb8b005
nicolascavalli <94708106+nicolascavalli@users.noreply.github.com>
2021-12-09 18:56:39 -0300

    Update recebedor.ino

--- Documents/TFG/recebedor/recebedor/recebedor.ino
+++ Documents/TFG/recebedor/recebedor/recebedor.ino
@@ -8,7 +8,7 @@ Serial.begin(9600);
 void loop() 
 {
   
-  if (Serial.available())                        // checking if data is received/available 
+  if (Serial.available())
   {                  
            a = Serial1.read();
            Serial2.println(a);
